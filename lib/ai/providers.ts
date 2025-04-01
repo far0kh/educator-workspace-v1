45cdb704a7f6a802da91a0866822d0dff55860b0
Farokh Madah <fa.madah@gmail.com>
2025-04-01 23:41:39 +0330

    Initial Vercel AI Chat

---
+++ lib/ai/providers.ts
@@ -0,0 +1,37 @@
+import {
+  customProvider,
+  extractReasoningMiddleware,
+  wrapLanguageModel,
+} from 'ai';
+import { google } from '@ai-sdk/google';
+import { isTestEnvironment } from '../constants';
+import {
+  artifactModel,
+  chatModel,
+  reasoningModel,
+  titleModel,
+} from './models.test';
+
+export const myProvider = isTestEnvironment
+  ? customProvider({
+    languageModels: {
+      'chat-model': chatModel,
+      'chat-model-reasoning': reasoningModel,
+      'title-model': titleModel,
+      'artifact-model': artifactModel,
+    },
+  })
+  : customProvider({
+    languageModels: {
+      'chat-model': google('gemini-2.0-flash'),
+      'chat-model-reasoning': wrapLanguageModel({
+        model: google('gemini-2.0-flash'),
+        middleware: extractReasoningMiddleware({ tagName: 'think' }),
+      }),
+      'title-model': google('gemini-2.0-flash'),
+      'artifact-model': google('gemini-2.0-flash'),
+    },
+    imageModels: {
+      'small-model': google('gemini-2.0-flash-exp-image-generation') as any,
+    },
+  });
