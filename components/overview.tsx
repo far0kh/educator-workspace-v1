e497545b3d17e23ae294a5e1fa89ef30157d655e
Farokh Madah <fa.madah@gmail.com>
2025-04-06 00:41:24 +0330

    Refactor Overview component; replace static content with dynamic rendering and remove unused constants

--- components/overview.tsx
+++ components/overview.tsx
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion';
-import Link from 'next/link';
-
-import { MessageIcon, VercelIcon } from './icons';
+import Image from "next/image";
+import { Pages } from "@/constants/en";
+import ReactMarkdown from 'react-markdown';
 
 export const Overview = () => {
   return (
@@ -14,38 +14,12 @@ export const Overview = () => {
       transition={{ delay: 0.5 }}
     >
       <div className="rounded-xl p-6 flex flex-col gap-8 leading-relaxed text-center max-w-xl">
-        <p className="flex flex-row justify-center gap-4 items-center">
-          <VercelIcon size={32} />
-          <span>+</span>
-          <MessageIcon size={32} />
-        </p>
-        <p>
-          This is an{' '}
-          <Link
-            className="font-medium underline underline-offset-4"
-            href="https://github.com/vercel/ai-chatbot"
-            target="_blank"
-          >
-            open source
-          </Link>{' '}
-          chatbot template built with Next.js and the AI SDK by Vercel. It uses
-          the{' '}
-          <code className="rounded-md bg-muted px-1 py-0.5">streamText</code>{' '}
-          function in the server and the{' '}
-          <code className="rounded-md bg-muted px-1 py-0.5">useChat</code> hook
-          on the client to create a seamless chat experience.
-        </p>
-        <p>
-          You can learn more about the AI SDK by visiting the{' '}
-          <Link
-            className="font-medium underline underline-offset-4"
-            href="https://sdk.vercel.ai/docs"
-            target="_blank"
-          >
-            docs
-          </Link>
-          .
-        </p>
+        <div className="flex flex-row justify-center gap-4 items-center">
+          <h1 className="text-4xl md:text-5xl font-black text-balance">{`${Pages.chat.welcome.hello} 👋`}</h1>
+        </div>
+        <div className="text-muted-foreground">
+          <ReactMarkdown>{Pages.chat.welcome.description}</ReactMarkdown>
+        </div>
       </div>
     </motion.div>
   );
